05c07482ebe128adc72bc12f70d25eb38f8d5c6c
Kehinde13 <balogunkehinde3@gmail.com>
2023-09-14 03:09:57 -0700

    change from class to functional  component

--- index.js
+++ index.js
@@ -1,70 +1,84 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
+import React, { useState, StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './style.css';
 
-class StopWatch extends Component {
-  state = {lapse: 0, running: false, display1: true};
-  
-  handleRunClick = () => {
-    this.setState(state => {
-      if(state.running) {
-        clearInterval(this.timer)
-      } else {
-        const startTime = Date.now() - this.state.lapse;
-        this.timer = setInterval(() => {
-          this.setState({
-            lapse: Date.now() - startTime
-          }, () => {console.log(this.state.lapse)})
-        })
-      }
-      return {running: !state.running}
-    })
-  }
+import React, { useState, useEffect } from 'react';
 
-  componentWillUnMount() {
+function StopWatch() {
+  const [lapse, setLapse] = useState(0);
+  const [running, setRunning] = useState(false);
 
-  }
+  let timer;
 
-  handleClearClick = () => {
-    clearInterval(this.timer)
-    this.setState({
-      lapse: 0,
-      running: false,
-    })
-  }
-
-  render() {
-    const {lapse, running} = this.state;
-
-    const buttonStyles = {
-      border: '1px solid #ccc',
-      background: '#fff',
-      fontSize: '2em',
-      padding: 25,
-      margin: 15,
-      width: 200,
+  const handleRunClick = () => {
+    if (running) {
+      clearInterval(timer);
+    } else {
+      const startTime = Date.now() - lapse;
+      timer = setInterval(() => {
+        setLapse(Date.now() - startTime);
+      });
     }
+    setRunning(!running);
+  };
+
+  const handleClearClick = () => {
+    clearInterval(timer);
+    setLapse(0);
+    setRunning(false);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const buttonStyles = {
+    border: '1px solid #ccc',
+    background: '#fff',
+    fontSize: '2em',
+    padding: 25,
+    margin: 15,
+    width: 200,
+  };
+
   return (
-    <div style={{textAlign: 'center'}} >
-      <label style={{fontSize: '5em', display: 'block'}} >{lapse}ms</label>
-      <button style={buttonStyles}
-      onClick={this.handleRunClick} >{running ? 'Stop' : 'Start'}</button>
-      <button style={buttonStyles}
-      onClick={this.handleClearClick} >Clear</button>
+    <div style={{ textAlign: 'center' }}>
+      <label style={{ fontSize: '5em', display: 'block' }}>{lapse}ms</label>
+      <button style={buttonStyles} onClick={handleRunClick}>
+        {running ? 'Stop' : 'Start'}
+      </button>
+      <button style={buttonStyles} onClick={handleClearClick}>
+        Clear
+      </button>
     </div>
-  )
-  }
+  );
+
+
+
 }
 
 
 function App() {
+  const [display1, setDisplay1] = useState(true);
+
+  const handleDisplay = () => {
+    setDisplay1(!display1)
+  }
   return (
     <div>
     Click here to hide
-    <input checked={} onClick={} type='checkbox' />
-    <StopWatch />
+    <input onChange={handleDisplay} type='checkbox' />
+     {display1 ?  <StopWatch /> : " "}
     </div>
   )
 }
-render(<App />, document.getElementById('root'));
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
